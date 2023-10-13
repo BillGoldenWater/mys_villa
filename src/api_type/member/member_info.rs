6c4f1c3b479ef55e89a88c3b0c29984d30882f02
Golden_Water <2439577268@qq.com>
2023-10-13 20:30:39 +0800

    fix(bot::api_type::member): fix deserialize error when user has custom avatar

--- src/api_type/member/member_info.rs
+++ src/api_type/member/member_info.rs
@@ -5,14 +5,19 @@
  */
 
 use serde::Deserialize;
-use serde_aux::field_attributes::deserialize_number_from_string;
+use serde_aux::field_attributes::{
+  deserialize_number_from_string, deserialize_option_number_from_string,
+};
 
 /// member information
 #[derive(Debug, Clone, Deserialize)]
 pub struct MemberInfo {
   /// avatar id
-  #[serde(rename = "avatar", deserialize_with = "deserialize_number_from_string")]
-  pub avatar_id: u64,
+  #[serde(
+    rename = "avatar",
+    deserialize_with = "deserialize_option_number_from_string"
+  )]
+  pub avatar_id: Option<u64>,
   /// avatar url
   pub avatar_url: String,
   /// self description
